5aaf0bf4afad9ee0bc5c0a48007f676df8aeb20d
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-02-08 23:33:28 -0500

    lab 6 added, includes many more design patterns such as Observable, MVC, Composite, Strategy, Command, Factory.

---
+++ lab06/paint/PaintPanel.java
@@ -0,0 +1,180 @@
+package ca.utoronto.utm.paint;
+
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+class PaintPanel extends StackPane implements Observer, EventHandler<MouseEvent> {
+
+	private int i = 0;
+	private PaintModel model; // slight departure from MVC, because of the way painting works
+	private View view; // So we can talk to our parent or other components of the view
+
+	private String mode; // modifies how we interpret input (could be better?)
+	private Circle circle; // the circle we are building
+
+	private Canvas canvas;
+
+	public PaintPanel(PaintModel model, View view) {
+
+		this.canvas = new Canvas(300, 300);
+		this.getChildren().add(this.canvas);
+		// The canvas is transparent, so the background color of the
+		// containing pane serves as the background color of the canvas.
+		this.setStyle("-fx-background-color: blue");
+
+		this.addEventHandler(MouseEvent.ANY, this);
+
+		this.mode = "Circle"; // bad code here?
+
+		this.model = model;
+		this.model.addObserver(this);
+
+		this.view = view;
+	}
+
+	public void repaint() {
+
+		GraphicsContext g = this.canvas.getGraphicsContext2D();
+
+		// Clear the canvas
+		g.clearRect(0, 0, this.getWidth(), this.getHeight());
+
+		g.setStroke(Color.WHITE);
+		g.strokeText("i=" + i, 50, 75);
+		i = i + 1;
+
+		// Draw Lines
+		ArrayList<Point> points = this.model.getPoints();
+		for (int i = 0; i < points.size() - 1; i++) {
+			Point p1 = points.get(i);
+			Point p2 = points.get(i + 1);
+			g.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+		}
+
+		// Draw Circles
+		ArrayList<Circle> circles = this.model.getCircles();
+		for (Circle c : circles) {
+			int x = c.getCentre().getX();
+			int y = c.getCentre().getY();
+			int radius = c.getRadius();
+			g.strokeOval(x, y, radius, radius);
+		}
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+
+		// Not exactly how MVC works, but similar.
+		this.repaint();
+	}
+
+	/**
+	 * Controller aspect of this
+	 */
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+	@Override
+	public void handle(MouseEvent event) {
+		if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+			mouseDragged(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+			mousePressed(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_MOVED) {
+			mouseMoved(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+			mouseClicked(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
+			mouseReleased(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+			mouseEntered(event);
+		} else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
+			mouseExited(event);
+		}
+	}
+
+	private void mouseMoved(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
+		}
+	}
+
+	private void mouseDragged(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+			this.model.addPoint(new Point((int) e.getX(), (int) e.getY()));
+		} else if (this.mode == "Circle") {
+
+		}
+	}
+
+	private void mouseClicked(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
+		}
+	}
+
+	private void mousePressed(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+			// Problematic notion of radius and centre!!
+			Point centre = new Point((int) e.getX(), (int) e.getY());
+			int radius = 0;
+			this.circle = new Circle(centre, radius);
+		}
+	}
+
+	private void mouseReleased(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+			if (this.circle != null) {
+				// Problematic notion of radius and centre!!
+				// WE DO THIS TO CLACULATE RADIUS
+				int shiftx = Math.abs((int) this.circle.getCentre().getX() - (int) e.getX());
+				int shifty = Math.abs((int) this.circle.getCentre().getY() - (int) e.getY());
+
+				int radius = (Math.max(shiftx, shifty));
+				this.circle.setRadius(2*radius);
+				// change x and y of center
+				int newX = this.circle.getCentre().getX() - radius;
+				int newY = this.circle.getCentre().getY() - radius;
+				Point newCentre = new Point(newX, newY);
+				// set it as new center 
+				this.circle.setCentre(newCentre);
+				this.model.addCircle(this.circle);
+				this.circle = null;
+			}
+		}
+
+	}
+
+	private void mouseEntered(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
+		}
+	}
+
+	private void mouseExited(MouseEvent e) {
+		if (this.mode == "Squiggle") {
+
+		} else if (this.mode == "Circle") {
+
+		}
+	}
+}
